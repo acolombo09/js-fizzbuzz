541da039c9baf3d823c4a7b93c5e0b77e47b781f
acolombo09 <acolombo0911@gmail.com>
2023-07-04 15:44:32 +0200

    style_features

--- js/main.js
+++ js/main.js
@@ -1,17 +1,33 @@
+/*
+**Consegna:**
+Scrivi un programma che stampi in console i numeri da 1 a 100,
+ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
+Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+**Prima di partire a scrivere codice poniamoci qualche domanda:**
+Come faccio a sapere se un numero è divisibile per un altro?
+Abbiamo visto qualcosa di particolare che possiamo usare?
+**Consigli del giorno:**
+1. Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+2. Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo “a mano”
+**BONUS 1:**
+Crea un container nel DOM , aggiungendo (attraverso la funzione `append()`) un elemento html con il numero o la stringa corretta da mostrare.
+**BONUS 2:**
+Applica stili differenti agli elementi aggiunti al DOM nel *BONUS 1*, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
+Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna.
+Buon lavoro e buon divertimento! :faccia_leggermente_sorridente:
+*/
+
 const chessContainer = document.querySelector(".chess-container");
-const totalCells = 8 * 8;
+const totalCells = 7 * 7;
 
 for (let i = 0; i < totalCells; i++) {
   // let singleCell = `<div class="chess-square"></div>`;
   let singleCell = document.createElement("div");
-  singleCell.classList.add("chess-square");
-  singleCell.innerHTML = i.toString();
+  singleCell.classList.add("chess-square", "m-2");
+  singleCell.innerHTML = (i + 1).toString();
   
-  if(i % 2 === 1){
-    singleCell.classList.add("bg-dark", "text-light");
-    singleCell.addEventListener("click", function(){
-      alert("Hai cliccato una cella scura!");
-    });
+  if(i % 3 === 1){
+    singleCell.classList.add("bg-dark");
   }
 
   chessContainer.append(singleCell);
